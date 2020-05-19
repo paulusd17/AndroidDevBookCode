757b75ac444815e17cda0fdd25f53fed4310041e
paulusd17 <paulusd17@students.ecu.edu>
2020-05-18 21:23:43 -0400

    Chapter 2

--- app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
@@ -4,12 +4,27 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
+    private lateinit var nextButton: Button
+    private lateinit var previousButton: Button
+    private lateinit var questionTextView: TextView
+
+    private val questionbank = listOf(
+        Question(R.string.question_africa,true),
+        Question(R.string.question_americas,true),
+        Question(R.string.question_asia,true),
+        Question(R.string.question_australia,true),
+        Question(R.string.question_mideast,true),
+        Question(R.string.question_oceans,true)
+    )
+
+    private var currentIndex = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,21 +32,50 @@ class MainActivity : AppCompatActivity() {
 
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
+        nextButton = findViewById(R.id.next_button)
+        previousButton = findViewById(R.id.previous_button)
+        questionTextView = findViewById((R.id.question_text_view))
 
         trueButton.setOnClickListener { view: View ->
-            Toast.makeText(
-                this,
-                R.string.correct_toast,
-                Toast.LENGTH_SHORT)
-                .show()
+            checkAnswer(true)
         }
 
         falseButton.setOnClickListener { view: View ->
-            Toast.makeText(
-                this,
-                R.string.incorrect_toast,
-                Toast.LENGTH_SHORT)
-                .show()
+            checkAnswer(false)
         }
+
+        nextButton.setOnClickListener {
+            setQuestionForward()
+        }
+
+        previousButton.setOnClickListener {
+            setQuestionBackward()
+        }
+
+        questionTextView.setOnClickListener {
+            setQuestionForward()
+        }
+
+        updateQuestion()
+    }
+
+    private fun setQuestionForward(){
+        currentIndex = currentIndex++ % questionbank.size
+        updateQuestion()
+    }
+
+    private fun setQuestionBackward(){
+        currentIndex = currentIndex-- % questionbank.size
+        updateQuestion()
+    }
+
+    private fun updateQuestion(){
+        questionTextView.setText((questionbank[currentIndex].textResId))
+    }
+
+    private fun checkAnswer(userAnswer : Boolean){
+        Toast.makeText(this,
+           if (userAnswer == questionbank[currentIndex].answer) R.string.correct_toast else R.string.incorrect_toast,
+        Toast.LENGTH_SHORT).show()
     }
 }
