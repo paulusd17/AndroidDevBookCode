d99df3ffe0af9a8b45edee08e43913fe03ca588f
Dillon Paulus <31076992+paulusd17@users.noreply.github.com>
2020-05-19 21:02:13 -0400

    end of chapter 3

--- app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
@@ -2,18 +2,22 @@ package com.bignerdranch.android.geoquiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+
+private const val TAG ="MainActivity"
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
     private lateinit var nextButton: Button
-    private lateinit var previousButton: Button
     private lateinit var questionTextView: TextView
+    private var Correct = 0
+
 
     private val questionbank = listOf(
         Question(R.string.question_africa,true),
@@ -28,6 +32,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate(Bundle?) called")
         setContentView(R.layout.activity_main)
 
         trueButton = findViewById(R.id.true_button)
@@ -54,16 +59,59 @@ class MainActivity : AppCompatActivity() {
         updateQuestion()
     }
 
+    override fun onStart(){
+        super.onStart()
+        Log.d(TAG, "onStart() called")
+    }
+
+    override fun onResume(){
+        super.onResume()
+        Log.d(TAG, "onResume() called")
+    }
+
+    override fun onPause(){
+        super.onPause()
+        Log.d(TAG, "onPause() called")
+    }
+
+    override fun onStop(){
+        super.onStop()
+        Log.d(TAG, "onStop() called")
+    }
+
+    override fun onDestroy(){
+        super.onDestroy()
+        Log.d(TAG, "onDestroy() called")
+    }
+
     private fun setQuestionForward(){
-        currentIndex = currentIndex++ % questionbank.size
-        updateQuestion()
+        if (currentIndex == questionbank.size-1) {
+            var temp = questionbank.size
+            Toast.makeText(
+                this,
+                "$Correct of $temp correct",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+        else {
+            currentIndex = (currentIndex + 1) % questionbank.size
+            updateQuestion()
+        }
     }
 
     private fun updateQuestion(){
+        trueButton.isClickable = true
+        falseButton.isClickable = true
         questionTextView.setText((questionbank[currentIndex].textResId))
     }
 
     private fun checkAnswer(userAnswer : Boolean){
+        trueButton.isClickable = false
+        falseButton.isClickable = false
+
+        if (userAnswer == questionbank[currentIndex].answer)
+            Correct += 1
+
         Toast.makeText(this,
            if (userAnswer == questionbank[currentIndex].answer) R.string.correct_toast else R.string.incorrect_toast,
         Toast.LENGTH_SHORT).show()
